bf905f3ebbffd61a38bd878aeba9d292c5a8b939
Miku <26039249+xMikux@users.noreply.github.com>
2023-05-21 15:25:16 +0800

    小調整

--- src/main/java/io/github/thebusybiscuit/slimefun4/implementation/StartupWarnings.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/implementation/StartupWarnings.java
@@ -36,11 +36,11 @@ final class StartupWarnings {
     @ParametersAreNonnullByDefault
     static void invalidMinecraftVersion(Logger logger, int majorVersion, String slimefunVersion) {
         logger.log(Level.SEVERE, BORDER);
-        logger.log(Level.SEVERE, PREFIX + "Slimefun 並未被正確安裝!");
-        logger.log(Level.SEVERE, PREFIX + "你正在使用不支持的Minecraft版本!");
+        logger.log(Level.SEVERE, PREFIX + "Slimefun 並未被正確安裝！");
+        logger.log(Level.SEVERE, PREFIX + "你正在使用不支援的 Minecraft 版本！");
         logger.log(Level.SEVERE, PREFIX);
-        logger.log(Level.SEVERE, PREFIX + "你正在使用Minecraft 1.{0}.x", majorVersion);
-        logger.log(Level.SEVERE, PREFIX + "但 Slimefun {0} 只支持", slimefunVersion);
+        logger.log(Level.SEVERE, PREFIX + "你正在使用 Minecraft 1.{0}.x", majorVersion);
+        logger.log(Level.SEVERE, PREFIX + "但 Slimefun {0} 只支援", slimefunVersion);
         logger.log(Level.SEVERE, PREFIX + "Minecraft {0}", String.join(" / ", Slimefun.getSupportedVersions()));
         logger.log(Level.SEVERE, BORDER);
     }
@@ -48,12 +48,12 @@ final class StartupWarnings {
     @ParametersAreNonnullByDefault
     static void invalidServerSoftware(Logger logger) {
         logger.log(Level.SEVERE, BORDER);
-        logger.log(Level.SEVERE, PREFIX + "Slimefun 並未被正確安裝!");
-        logger.log(Level.SEVERE, PREFIX + "CraftBukkit 不再受支持!");
+        logger.log(Level.SEVERE, PREFIX + "Slimefun 並未被正確安裝！");
+        logger.log(Level.SEVERE, PREFIX + "CraftBukkit 已不再受支援！");
         logger.log(Level.SEVERE, PREFIX);
-        logger.log(Level.SEVERE, PREFIX + "Slimefun 要求你使用 Spigot, Paper 或");
-        logger.log(Level.SEVERE, PREFIX + "任何Spigot或Paper的受支持分支.");
-        logger.log(Level.SEVERE, PREFIX + "(我們推薦Paper)");
+        logger.log(Level.SEVERE, PREFIX + "Slimefun 需要你使用 Spigot、Paper 或");
+        logger.log(Level.SEVERE, PREFIX + "任何 Spigot 或 Paper 的受支援分支。");
+        logger.log(Level.SEVERE, PREFIX + "（我們推薦 Paper）");
         logger.log(Level.SEVERE, BORDER);
     }
 
@@ -62,14 +62,14 @@ final class StartupWarnings {
         int javaVersion = NumberUtils.getJavaVersion();
 
         logger.log(Level.WARNING, BORDER);
-        logger.log(Level.WARNING, PREFIX + "你的 Java 版本 (Java {0}) 已過時.", javaVersion);
+        logger.log(Level.WARNING, PREFIX + "你的 Java 版本（Java {0}）已過時。", javaVersion);
         logger.log(Level.WARNING, PREFIX);
-        logger.log(Level.WARNING, PREFIX + "我們推薦你升級至 Java {0}.", recommendedJavaVersion);
+        logger.log(Level.WARNING, PREFIX + "我們推薦你升級到 Java {0}。", recommendedJavaVersion);
         logger.log(Level.WARNING, PREFIX + "在新版本 Minecraft 中將需要使用 Java {0}", recommendedJavaVersion);
         logger.log(Level.WARNING, PREFIX + "我們希望能盡快利用");
-        logger.log(Level.WARNING, PREFIX + "它所帶來的新功能.");
-        logger.log(Level.WARNING, PREFIX + "在可預見的未來, Slimefun 也將需要 Java {0}", recommendedJavaVersion);
-        logger.log(Level.WARNING, PREFIX + "所以請更新!");
+        logger.log(Level.WARNING, PREFIX + "它所帶來的新功能。");
+        logger.log(Level.WARNING, PREFIX + "在可預見的未來，Slimefun 也將需要 Java {0}", recommendedJavaVersion);
+        logger.log(Level.WARNING, PREFIX + "所以請更新！");
         logger.log(Level.WARNING, BORDER);
     }
 
